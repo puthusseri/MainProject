85c567ff1d682f864a661999479785b3743447ca
Vyshak Puthusseri <vyshakputhusseri@gmail.com>
2020-01-20 18:22:39 +0530

    feat:Stemming was done
    The process helps to convert the vocabolury to the root word

---
+++ Studying/a.py
@@ -0,0 +1,12 @@
+import nltk
+nltk.download('punkt')
+from nltk.stem import PorterStemmer 
+from nltk.tokenize import word_tokenize 
+   
+ps = PorterStemmer() 
+   
+sentence = "Programers program with programing languages working works unworking unhappy"
+words = word_tokenize(sentence) 
+   
+for w in words: 
+    print(w, " : ", ps.stem(w)) 
