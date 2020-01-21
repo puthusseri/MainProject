13c63a19ebbc4a022d3ab3e4d7d124b505c0e004
Vyshak Puthusseri <vyshakputhusseri@gmail.com>
2020-01-21 18:38:16 +0530

    feat:Basic text processing
    Loaded the model and used spacy to do basic nlp.

--- Studying/a.py
+++ Studying/a.py
@@ -1,12 +1,11 @@
+
+#Stemming
 import nltk
 nltk.download('punkt')
-from nltk.stem import PorterStemmer 
+from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize 
-   
 ps = PorterStemmer() 
-   
 sentence = "Programers program with programing languages working works unworking unhappy"
 words = word_tokenize(sentence) 
-   
-for w in words: 
+for w in words:
     print(w, " : ", ps.stem(w)) 
