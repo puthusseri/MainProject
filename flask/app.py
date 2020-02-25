e92e657ea77053a12e99154555c577b5aedfa3fc
Vyshak Puthusseri <vyshakputhusseri@gmail.com>
2020-02-25 14:18:23 +0530

    feat:The json object was able to transfer to the html
    But not able to parse it

--- flask/app.py
+++ flask/app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template, request      
 import sys
-
+import maincode as maincode
 app = Flask(__name__)
 
 @app.route("/generateMCQ")
 def generatemcq():
 	return render_template("home.html")
+	
+	
+@app.route("/a")
+def generatemcqaa():
+        a= maincode.returnQuestionSetJSON('My friends are eating in the palace.')
+        return render_template("home.html",abc=a)
 @app.route("/")
 def home():
 	return render_template("a.html")
