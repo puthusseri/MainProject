9e7e822a583e63fb28754d4732fb3eff588ec755
Vyshak Puthusseri <vyshakputhusseri@gmail.com>
2020-06-26 00:57:16 +0530

    chore: Created a new UI and integrated both tasks
    Before the task ot two types of question generation was 2 different
    program.

--- projectPresentation/README.md
+++ projectPresentation/README.md
@@ -4,3 +4,10 @@
 - Train.csv
 - Test.csv
 - model folder
+
+- [x] UI
+- [x] Integrate both tasks of generating question
+- [] Use glove 300D rather than 100D
+- [] Retrain the network using 300D
+- [] Make presentation
+- [] try generating true or flase questions
--- projectPresentation/app.py
+++ projectPresentation/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request      
 import sys
 import demo as d
+import maincode as maincode
 app = Flask(__name__)
+
 
 @app.route("/generateMCQ", methods=["POST"])
 def setee():
@@ -14,11 +16,15 @@ def setee():
         d.ansewringQuestions()
         d.generateDistractors()
         a = d.questions_answers_distractors_list
+        li = maincode.generateOtherTypeQue()
+        return render_template("ans.html",abc=a,length = len(a), li = li, l = len(li) ) 
 
-        return render_template("ans.html",abc=a,length = len(a)) 
-
-@app.route("/")
+@app.route("/home")
 def home():
+        return render_template("vyshak.html")
+       
+@app.route("/")
+def hometemp():
         return render_template("vyshak.html")
 
 if __name__ == "__main__":
